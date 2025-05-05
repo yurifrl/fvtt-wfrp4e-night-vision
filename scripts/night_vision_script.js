8bc3c8387a8c601bb53dd18ef472425a83caf2fa
TheFirst05 <96636889+TheFirst05@users.noreply.github.com>
2025-05-05 11:05:10 +0200

    Update night_vision_script.js
    Add disable toggles

--- scripts/night_vision_script.js
+++ scripts/night_vision_script.js
@@ -53,7 +53,117 @@ Hooks.on('init', () => {
 			);
 		},
 	});
+
+	/*
+	game.settings.register("wfrp4e-night-vision", "disable", {
+		name: "Night Vision active by default",
+		hint: "Once clicked, adds a flag to disable",
+		scope: 'world',
+		config: true,
+		type: Boolean,
+		default: false,
+	});
+	*/
+
 });
+
+
+
+Hooks.on('renderSceneConfig', async (obj) => {
+	// return if this scene document isn't editable
+	if (!obj.isEditable) return;
+
+	// Set the default status of a scene
+	if (!foundry.utils.hasProperty(obj.document, 'flags.wfrp4e-night-vision.disable')) {
+		await obj.document.setFlag('wfrp4e-night-vision', 'disable', false);
+	}
+
+	const disableNightVision = obj.document.getFlag('wfrp4e-night-vision', 'disable') ? 'checked' : '';
+	// Build our new options.
+	const injection = `
+	  <fieldset class="nv-scene-config">
+		<div class="form-group">
+		  <label>Disable Night Vision</label>
+		  <input
+			type="checkbox"
+			name="flags.wfrp4e-night-vision.disable"
+			${disableNightVision}>
+		  <p class="hint">Disable Night Vision functionality on this scene</p>
+		</div>
+		</fieldset>`;
+
+	if ($(obj.form).find('.nv-scene-config').length === 0) {
+		$(obj.form)
+			.find('p:contains("' + game.i18n.localize('SCENE.FIELDS.environment.darknessLock.hint') + '")')
+			.parent()
+			.after(injection);
+	}
+	// Re-auto-size the app window.
+	obj.setPosition();
+
+});
+
+
+Hooks.on('renderAmbientLightConfig', async (obj) => {
+	
+	const light = obj.document;
+
+	// Create checkbox HTML element
+	const bf = new foundry.data.fields.BooleanField();
+
+	/** @type {Element} */
+
+	const el = bf.toFormGroup(
+		{
+			label: "Disable Night Vision",
+			hint: "Night Vision ignores this light source",
+		},
+		{
+			name: "flags.wfrp4e-night-vision.disable",
+			value: light.getFlag('wfrp4e-night-vision', 'disable') ?? false,
+		}
+	);
+
+	// Create containing fieldset
+	const field = document.createElement("fieldset");
+	const legend = document.createElement("legend");
+	legend.innerText = "WFRP4e Night Vision";
+	field.append(legend, el);
+
+	// Insert new checkbox
+	$(obj.form).find('section.tab[data-tab="advanced"]').append(field);
+});
+
+
+Hooks.on("renderTokenConfig", async (obj) => {
+	const light = obj.document;
+
+	// Create checkbox HTML element
+	const bf = new foundry.data.fields.BooleanField();
+
+	/** @type {Element} */
+
+	const el = bf.toFormGroup(
+		{
+			label: "Disable Night Vision",
+			hint: "Night Vision ignores this light source",
+		},
+		{
+			name: "flags.wfrp4e-night-vision.disable",
+			value: light.getFlag('wfrp4e-night-vision', 'disable') ?? false,
+		}
+	);
+
+	// Create containing fieldset
+	const field = document.createElement("fieldset");
+	const legend = document.createElement("legend");
+	legend.innerText = "WFRP4e Night Vision";
+	field.append(legend, el);
+
+	// Insert new checkbox
+	$(obj.form).find('.tab[data-tab="light"]').append(field);
+});
+
 
 
 //define the values that calculate the Nightvision multiplier, and make them 0 so they don't introduce undefined into the calculation
@@ -61,78 +171,101 @@ let multiplier = { dim: 0, bright: 0 };
 let nightVisionDistance = 0;
 let distancePix = 0;
 
-//Thus us where the magic happens
-const mixin = Base => class extends Base {
-	/** @override */
-	_getLightSourceData() {
-		const data = super._getLightSourceData();
 
-		data.dim += multiplier.dim * nightVisionDistance * distancePix;
+//This is where the magic happens
+const mixin = Base =>
+	class extends Base {
+		/** @override */
+		_getLightSourceData() {
+			const data = super._getLightSourceData();
 
-		if (game.settings.get("wfrp4e-night-vision", "nightVisionBright")) {
-			data.bright += multiplier.bright * nightVisionDistance / 2 * distancePix;
+			const { dim, bright } = this.getRadius(data.dim, data.bright);
+
+			// Avoid NaN and introducing keys that shouldn't be in the data
+			// Without undefined check, global illumination will cause darkvision and similar vision modes to glitch.
+			// We're assuming getRadius gives sensible values otherwise.
+			if (data.dim !== undefined) data.dim = dim;
+			if (data.bright !== undefined) data.bright = bright;
+
+			return data;
+
 		}
-		return data;
-	}
-};
 
-const visionCalculator = function () {
-	multiplier = { dim: 0, bright: 0 }; //reset multiplier to 0 (erases previous values)
-	const gmSelection = game.user.isGM || game.settings.get("wfrp4e-night-vision", "onlyOnSelection");
-	const controlledtoken = canvas.tokens.controlled;
-	let relevantTokens; // define which tokens with which to calculate Night Vision settings.
+		getRadius(dim, bright) {
+			const result = { dim, bright };
+			multiplier = { dim: 0, bright: 0 }; //reset multiplier to 0 (erases previous values)
 
-	/* 
-	If a player has a token controlled, only define Night Vision based on those tokens.
-	If a player does not control a token, define Night Vision based on all their tokens.
-	GMs only define Night Vision based on tokens controlled.
-	*/
-	if (controlledtoken.length) {//if a token controlled, collect all tokens that you are controlling that you have vision for
-		relevantTokens = canvas.tokens.placeables.filter(
-			(o) =>
-				!!o.actor && o.actor?.testUserPermission(game.user, "OBSERVER") && o.controlled
-		);
-	} else { //if no tokens controlled, then check all tokens for which you have vision. this doesn't apply for GM
-		relevantTokens = canvas.tokens.placeables.filter(
-			(o) =>
-				!!o.actor && o.actor?.testUserPermission(game.user, "OBSERVER") && (gmSelection ? o.controlled : true)
-		);
+			if (this.document.getFlag('wfrp4e-night-vision', 'disable') === true) return result;
+
+			const gmSelection = game.user.isGM || game.settings.get("wfrp4e-night-vision", "onlyOnSelection");
+			const controlledtoken = canvas.tokens.controlled;
+			let relevantTokens; // define which tokens with which to calculate Night Vision settings.
+
+			/* 
+			If a player has a token controlled, only define Night Vision based on those tokens.
+			If a player does not control a token, define Night Vision based on all their tokens.
+			GMs only define Night Vision based on tokens controlled.
+			*/
+			if (controlledtoken.length) {//if a token controlled, collect all tokens that you are controlling that you have vision for
+				relevantTokens = canvas.tokens.placeables.filter(
+					(o) =>
+						!!o.actor && o.actor?.testUserPermission(game.user, "OBSERVER") && o.controlled
+				);
+			} else { //if no tokens controlled, then check all tokens for which you have vision. this doesn't apply for GM
+				relevantTokens = canvas.tokens.placeables.filter(
+					(o) =>
+						!!o.actor && o.actor?.testUserPermission(game.user, "OBSERVER") && (gmSelection ? o.controlled : true)
+				);
+			};
+
+			if (gmSelection && relevantTokens.length) {
+				// GM mode: only sees Night Vision if 100% selected tokens have Night Vision (and only the worst).
+				// At least one token must be selected for Night Vision to trigger (otherwise it's default foundry behaviour).
+				multiplier = { dim: 999, bright: 999 };
+
+				for (const t of relevantTokens) {
+					const tokenVision = t.actor.items.filter(i => i.name.includes(game.i18n.localize("NAME.NightVision")));
+					multiplier.dim = Math.min(multiplier.dim, tokenVision.length);
+					multiplier.bright = Math.min(multiplier.bright, tokenVision.length);
+				}
+			} else {
+				// Player mode: sees best Night Vision of all owned tokens.
+				for (const t of relevantTokens) {
+					const tokenVision = t.actor.items.filter(i => i.name.includes(game.i18n.localize("NAME.NightVision")));
+					multiplier.dim = Math.max(multiplier.dim, tokenVision.length);
+					multiplier.bright = Math.max(multiplier.bright, tokenVision.length);
+				}
+			}
+
+			// Define the values for nightvision distance
+			distancePix = game.scenes.viewed.dimensions.distancePixels; // find the pixels per grid unit (assume it's yards)
+			nightVisionDistance = game.settings.get("wfrp4e-night-vision", "nightVisionDistance");
+
+			//console.log("LOOK HERE", multiplier);
+
+			if (game.scenes.viewed.getFlag('wfrp4e-night-vision', 'disable') === false) {
+
+				result.dim += multiplier.dim * nightVisionDistance * distancePix;
+
+				if (game.settings.get("wfrp4e-night-vision", "nightVisionBright")) {
+					result.bright += multiplier.bright * nightVisionDistance / 2 * distancePix;
+				}
+			}
+
+
+
+			return result;
+		}
 	};
 
-	if (gmSelection && relevantTokens.length) {
-		// GM mode: only sees Night Vision if 100% selected tokens have Night Vision (and only the worst).
-		// At least one token must be selected for Night Vision to trigger (otherwise it's default foundry behaviour).
-		multiplier = { dim: 999, bright: 999 };
-
-		for (const t of relevantTokens) {
-			const tokenVision = t.actor.items.filter(i => i.name.includes(game.i18n.localize("NAME.NightVision")));
-			multiplier.dim = Math.min(multiplier.dim, tokenVision.length);
-			multiplier.bright = Math.min(multiplier.bright, tokenVision.length);
-		}
-	} else {
-		// Player mode: sees best Night Vision of all owned tokens.
-		for (const t of relevantTokens) {
-			const tokenVision = t.actor.items.filter(i => i.name.includes(game.i18n.localize("NAME.NightVision")));
-			multiplier.dim = Math.max(multiplier.dim, tokenVision.length);
-			multiplier.bright = Math.max(multiplier.bright, tokenVision.length);
-		}
-	}
-
-	// Define the values for nightvision distance
-	distancePix = game.scenes.viewed.dimensions.distancePixels; // find the pixels per grid unit (assume it's yards)
-	nightVisionDistance = game.settings.get("wfrp4e-night-vision", "nightVisionDistance");
-
-	//console.log("LOOK HERE", multiplier, result);
-
-	// Refresh lighting to (un)apply Night Vision parameters to them
+const reinitLights = function () {
 	for (const { object } of canvas.effects.lightSources) {
 		if (!((object instanceof AmbientLight) || (object instanceof Token))) continue;
 		object.initializeLightSource();
 	}
+}
 
-};
-
-const Debouncer = foundry.utils.debounce(visionCalculator, 10);
+const Debouncer = foundry.utils.debounce(reinitLights, 10);
 
 Hooks.on("controlToken", () => {
 	Debouncer();
